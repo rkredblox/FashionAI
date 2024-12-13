8ccc4f194a048467219577fff6a18e50e5d89a31
rkredblox <radhakrishnan.g@redblox.io>
2024-12-13 11:38:27 +0530

    add cors origin

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI 
 # import config
 from api.API_v1.api import api_router
-
+from fastapi.middleware.cors import CORSMiddleware
+import config
 
 app = FastAPI(
      title='FastAPI-Customize template',
     #  description= config.app_description,
      version="1.0.0",
 
+)
+# Add CORS Middleware
+app.add_middleware(
+    CORSMiddleware,
+#     allow_origins=["http://localhost:8080"],  # Allow requests from the frontend's origin
+    allow_origins=config.origins,
+    allow_credentials=True,  # Allow cookies to be sent along with requests
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Allow all headers
 )
 
 @app.get('/')
